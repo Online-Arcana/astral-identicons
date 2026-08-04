6802b85d76c071c83dcfc612c348f966b6f4a3bc
kitty <141717449+kitty-crow@users.noreply.github.com>
2026-08-04 10:53:26 +0100

    Remove external runtime from development server

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,7 @@ const assets = fileAssets(assetsRoot);
 const sourceIndex = await Bun.file(`${root}/public/index.html`).text();
 const index = page(sourceIndex, {
   script: "/app.js",
-  stylesheet: "/responsive.css",
-  opencv: "https://docs.opencv.org/4.x/opencv.js"
+  stylesheet: "/responsive.css"
 });
 const responsiveStyle = await Bun.file(`${root}/public/responsive.css`).text();
 const build = await Bun.build({
